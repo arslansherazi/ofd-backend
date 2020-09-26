6807dfc9e19c1cdb89818103ff1945829c0267a1
arslansherazi <arslanhaidersherazi@hotmail.com>
2020-09-27 03:27:59 +0500

    update architecture

--- scripts/production/order_status_cron_job.py
+++ scripts/production/order_status_cron_job.py
@@ -3,10 +3,11 @@ import sys
 
 import schedule
 
-PACKAGE_PARENT = '..'
+from wrappers.py_sql import PySQL
+
+PACKAGE_PARENT = '../../'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-from wrappers.py_sql import PySQL
 
 
 def handle_placed_order_status(py_sql):
