443b0d15f5604c7df0cbef8869c0e010e1cde88c
Arslan <arslanhaider.dev@hotmail.com>
2024-06-26 01:16:27 +0500

    integrate load env

--- security/security_credentials.py
+++ security/security_credentials.py
@@ -1,16 +1,19 @@
+from dotenv import load_dotenv
 from Crypto.Cipher import AES
 
+load_dotenv()
+
 basic_auth_credentials = {
-    'username': 'lpHaNdeS',
-    'password': 'KdaAdL$KKJ$Sgf!%ebWp'
+    'username': os.environ('BASIC_AUTH_USERNAME'),
+    'password': os.environ('BASIC_AUTH_PASSWORD')
 }
 keys = {
-    'password_hash_key': 'hh%%as3!!@89ujh$$%^11PPDD&&))@@hujnb345'
+    'password_hash_key': os.environ('PASSWORD_HASH_KEY')
 }
-ENCRYPTION_KEY = 'e1430400ccbb40e181556775f70d3f56'
-ENCRYPTION_SALT = 'd2507dca39e14029'
+ENCRYPTION_KEY = os.environ('ENCRYPTION_KEY')
+ENCRYPTION_SALT = os.environ('ENCRYPTION_SALT')
 ENCRYPTION_MODE = AES.MODE_CBC
-ENCRYPTION_DISABLE_KEY = 'fgresdc456tgde%rf##&gfd123swedDfrt'
-SECRET_KEY = 'iZMWwABEI2WaLOkb4tkocKG3_YzaaXO1JB2gLxO9KgY='
-AWS_SECRET_ACCESS_KEY = 'PALNWQz3LNOcQrjiCCYzzojbzj6Xv5ImLh/VdDXc'
-AWS_ACCESS_KEY_ID = 'AKIATFPQJWU3SU5S5SEV'
+ENCRYPTION_DISABLE_KEY = os.environ('ENCRYPTION_DISABLE_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID')
